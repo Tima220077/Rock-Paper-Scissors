168942ec8f228a3b9278e227821f7763e1f0a3c5
Tima220077 <132214117+Tima220077@users.noreply.github.com>
2024-04-05 21:00:52 +0400

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,6 @@ function generateGame(e){
     getRandom()
     let emjBtn = e.target.id
     let newEmj = emjBtn + text
-    if (text == emjBtn) {
-        commentEl.textContent += "draw"
-    }
     // if (newEmj == "rs" ) {
         
     //     score1El.textContent = newEmj
@@ -32,10 +29,32 @@ function generateGame(e){
         case "pr":
         case "sp":
             console.log("win")
+            commentEl.textContent = "You won"
+            score1++
+        //winning case
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            console.log("lost")
+            //losing case
+            commentEl.textContent = "You lost"
+            score2++
+            break;
+        case "rr":
+        case "ss":
+        case "pp":
+           console.log("draw")
+           commentEl.textContent = "It's a draw"
+           score1++
+           score2++
+           break;
     }
-    score1El.textContent = newEmj
+    score1El.textContent = score1
+    score2El.textContent = score2
 }
 function getRandom(){
     let randomE = Math.floor(Math.random()*3)
     text = arrEmj[randomE]
 }
+
